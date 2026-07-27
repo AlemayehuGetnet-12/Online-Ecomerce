caa5d8090040ead02d0574b9abf7071e8136b6a1
Alemayehu Getnet <alexgetnet34l@example.com>
2026-07-27 04:34:44 -0500

    the complet loade page

---
+++ client/src/components/common/Loader.jsx
@@ -0,0 +1,26 @@
+const Loader = ({ fullScreen = false, size = 'md' }) => {
+  const sizeClasses = {
+    sm: 'w-4 h-4 border-2',
+    md: 'w-8 h-8 border-4',
+    lg: 'w-12 h-12 border-4',
+    xl: 'w-16 h-16 border-4',
+  }
+
+  const spinner = (
+    <div
+      className={`${sizeClasses[size]} border-gray-200 border-t-primary-600 rounded-full animate-spin`}
+    ></div>
+  )
+
+  if (fullScreen) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        {spinner}
+      </div>
+    )
+  }
+
+  return spinner
+}
+
+export default Loader
